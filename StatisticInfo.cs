5995fad79ba1491b598a815f881abbe2bd59779d
Duvan Albeiro Mejia Cortes - Junior Developer L1 <dmejia@indigo.tech>
2020-12-03 15:09:31 -0500

    first commit

---
+++ StatisticInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using DevExpress.Xpo;
+
+public class StatisticInfo : XPLiteObject
+{
+    public StatisticInfo(Session session)
+        : base(session)
+    {
+    }
+    Guid key;
+    [Key(true)]
+    public Guid Key
+    {
+        get { return key; }
+        set { SetPropertyValue(nameof(Key), ref key, value); }
+    }
+    string info;
+    [Size(255)]
+    public string Info
+    {
+        get { return info; }
+        set { SetPropertyValue(nameof(Info), ref info, value); }
+    }
+    DateTime date;
+    public DateTime Date
+    {
+        get { return date; }
+        set { SetPropertyValue(nameof(Date), ref date, value); }
+    }
+}
